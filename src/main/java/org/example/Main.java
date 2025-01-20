40a196e5aeb3a7294c35fff03dd8b9480c824a45
Igo Marcelino <geslucian@hotmail.com>
2025-01-20 11:55:18 -0300

    fix: adiciona o header para autorizar o uso da api

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,17 +8,19 @@ import java.net.http.HttpResponse;
 public class Main {
     public static void main(String[] args)  {
 
-            // cria uma requisicao do tipo GET
-            HttpRequest httpRequest = HttpRequest.
-                    newBuilder().
-                    uri(URI.create("")).
-                    build();
-            // cria uma instancia do http cliente
-            HttpClient httpClient = HttpClient.newHttpClient();
-            // enviando a requisicao de forma assincrona e esperando a resposta
-            String body = httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString()).
-                    thenApply(HttpResponse::body).join();
-            // imprime no console a resposta obtida
-            System.out.println(body);
+        // cria uma requisicao do tipo GET
+        HttpRequest httpRequest = HttpRequest.
+                newBuilder().
+                uri(URI.create("https://api.themoviedb.org/3/trending/movie/day?language=pt-BR")).
+                header("Authorization", "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMzBlOTY4YjJkYTg1YzhlZGNiNjI2YTM1ZmJjZTkzZiIsIm5iZiI6MTczNzM4MTE4Mi4zNzY5OTk5LCJzdWIiOiI2NzhlNTUzZWRiY2ZmMzNhOWM2NTU3YTYiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0._jMtPU8KDGEj8gj-ytwUBJlYuA2K2TGKVq5lPqYiDzk").// autorizacao para visualizacao da api
+                header("accept", "application/json").
+                build();
+        // cria uma instancia do http cliente
+        HttpClient httpClient = HttpClient.newHttpClient();
+        // enviando a requisicao de forma assincrona e esperando a resposta
+        String body = httpClient.sendAsync(httpRequest, HttpResponse.BodyHandlers.ofString()).
+                thenApply(HttpResponse::body).join();
+        // imprime no console a resposta obtida
+        System.out.println(body);
     }
 }
